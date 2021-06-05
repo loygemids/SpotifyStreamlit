6bd02bcb1e1c926de2e4d0e9e0adf57a9108e455
loygemids <84379434+loygemids@users.noreply.github.com>
2021-06-05 14:13:18 +0800

    Update Spotifyv1.py

--- Spotifyv1.py
+++ Spotifyv1.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import folium
 import seaborn as sns
 from streamlit_folium import folium_static
 import warnings
